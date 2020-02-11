7c51376357826f05c1130b0a2c267be50e1b3b6a
joelethan <joelkatsme@gmail.com>
2020-02-10 16:39:08 -0800

    Implement signup without Redux Add error handling in the signup form

--- src/component/auth/register.js
+++ src/component/auth/register.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import axios from 'axios';
 
 export default class Register extends Component {
     constructor(){
@@ -14,7 +16,7 @@ export default class Register extends Component {
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
-    
+
     onChange(e){
         this.setState({[e.target.name]: e.target.value })
     }
@@ -22,9 +24,15 @@ export default class Register extends Component {
         e.preventDefault()
         console.log(this.state);
         
+        axios.post('http://localhost:5001/api/user/register', this.state)
+            .then(res => console.log(res.data))
+            .catch(err => this.setState({errors: err.response.data}))
     }
 
     render() {
+        const { errors } =this.state;
+        console.log(Boolean(errors.name), '>>');
+
         return (
             <div className="register">
               <div className="container">
@@ -36,39 +44,43 @@ export default class Register extends Component {
                       <div className="form-group">
                         <input
                         type="text"
-                        className="form-control form-control-lg"
+                        className={classnames("form-control form-control-lg", {"is-invalid": errors.name})}
                         placeholder="Name"
                         name="name"
                         value={this.state.name}
                         onChange={this.onChange} />
+                        {errors.name && (<div className="invalid-feedback">errors.name</div>)}
                       </div>
                       <div className="form-group">
                         <input
                         type="email"
-                        className="form-control form-control-lg"
+                        className={classnames("form-control form-control-lg", {"is-invalid": errors.email})}
                         placeholder="Email Address"
                         name="email"
                         value={this.state.email}
                         onChange={this.onChange} />
+                        {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                         <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                       </div>
                       <div className="form-group">
                         <input
                         type="password"
-                        className="form-control form-control-lg"
+                        className={classnames("form-control form-control-lg", {"is-invalid": errors.password})}
                         placeholder="Password"
                         name="password"
                         value={this.state.password}
                         onChange={this.onChange} />
+                        {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                       </div>
                       <div className="form-group">
                         <input
                         type="password"
-                        className="form-control form-control-lg"
+                        className={classnames("form-control form-control-lg", {"is-invalid": errors.password2})}
                         placeholder="Confirm Password"
                         name="password2"
                         value={this.state.password2}
                         onChange={this.onChange} />
+                        {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
                       </div>
                       <input type="submit" className="btn btn-info btn-block mt-4" />
                     </form>
